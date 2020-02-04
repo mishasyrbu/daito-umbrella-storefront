dde9eff7a2c98ba05ac1f2baca3b8454da9afc05
Misha Syrbu <mihaisyrbu91@gmail.com>
2020-02-04 22:52:05 +0200

    config poke

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config({
   path: `.env`,
 })
 module.exports = {
-  pathPrefix: '/daito-umbrella-storefront',
+  // pathPrefix: '/daito-umbrella-storefront',
   plugins: [
     {
       resolve: '@gatsbystorefront/gatsby-theme-storefront-shopify',
